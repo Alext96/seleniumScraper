0663fe14bf85c25df4919d4627b80d6c4d2d5b99
Alext96 <alextarberg96@gmail.com>
2019-03-08 13:59:00 +0100

    Update SeleniumScraper.py

--- SeleniumScraper.py
+++ SeleniumScraper.py
@@ -4,26 +4,30 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-x = 2
+x = 1
 
 #opens chrome
-driver = webdriver.Chrome(r"C:\Users\Swagmaster\PycharmProjects\chromedriver_win32\chromedriver.exe")
-driver.get("https://www.spreadshirt.com/quotes+gifts")
+driver = webdriver.Chrome(r"Z:\Chromedriver\chromedriver.exe")
+driver.get("https://www.spreadshirt.com/things+to+do+today")
 
 #first time requires exiting popus
-driver.find_element_by_xpath("""//*[@id="sprd-main"]/div[7]/div[2]/div[2]/div[3]/div[1]/img""").click()
-driver.find_element_by_xpath("""//*[@id="articleTileList"]/div[1]/div/a/img""").click()
+
+driver.find_element_by_xpath("""//*[@id="sprd-main"]/div[5]/div[2]/div[2]/div[3]/div[1]/img""").click()
 sleep(3)
-driver.find_element_by_xpath("""//*[@id="navigation"]/div[3]/div[2]/span""").click()
-img = driver.find_element_by_xpath("""//*[@id="designDetailsPanel"]/div[1]/img""")
+driver.find_element_by_xpath("""//*[@id="navigation"]/div[4]/div[2]/span""").click()
+sleep(3)
+driver.find_element_by_xpath("""//*[@id="l-i"]""").click()
+sleep(3)
+#driver.find_element_by_xpath("""//*[@id="navigation"]/div[3]/div[2]/span""").click()
+#img = driver.find_element_by_xpath("""//*[@id="d-iO"]""")
 driver.back()
 
 #loop for navigating the site
 def navigate(x):
-    while x < 55:
+    while x < 30:
         x += 1
         #sleep(3)
-        driver.find_element_by_xpath("""//*[@id="articleTileList"]/div[""" + str(x) + """]/div/a/img""").click()
+        driver.find_element_by_xpath("""//*[@id="articleTileList"]/div[""" + str(x) + """]/a/div[1]/div[1]/img""").click()
         #sleep(3)
         downloadImage(x)
         #sleep(3)
@@ -32,7 +36,7 @@ def navigate(x):
 
 #downloading image, giving it random number
 def downloadImage(x):
-    img = driver.find_element_by_xpath("""//*[@id="designDetailsPanel"]/div[1]/img""")
+    img = driver.find_element_by_xpath("""//*[@id="d-iZ"]""")
     src = img.get_attribute('src')
     name = random.randrange(1, 100000)
     full_name = str(name) + ".png"
