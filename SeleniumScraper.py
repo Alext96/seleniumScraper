239fee9cf9c73611a8f65e1878f067f6ff5e2b74
Alext96 <alextarberg96@gmail.com>
2021-12-27 20:49:31 +0100

    Update SeleniumScraper.py
    improved code, still sucks

--- SeleniumScraper.py
+++ SeleniumScraper.py
@@ -4,10 +4,10 @@ import urllib.request
 from time import sleep
 import cv2
 import numpy as np
-import soup as soup
 import wget
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException, \
+    ElementClickInterceptedException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -16,23 +16,26 @@ x = 1
 
 # opens chrome
 driver = webdriver.Chrome("chromedriver.exe")
-driver.get("https://www.spreadshirt.com/bestselling+gifts?page=11")
+driver.get("https://www.spreadshirt.com/shop/user/qe-design/clothing/t-shirts/?sort=0")
 
 # first time requires exiting popus
+sleep(5)
+#try:
+    #driver.find_element_by_xpath("""//*[@id="sprd-main"]/div[4]/div[2]/div[1]""").click()
+#except NoSuchElementException:
+#    print("hittade inte 1")
 sleep(3)
 try:
-    driver.find_element_by_xpath("""//*[@id="sprd-main"]/div[4]/div[2]/div[1]""").click()
+    driver.find_element_by_xpath("""//*[@id="sprd-main"]/div[6]/div[2]/div/div[3]/div[1]/img""").click()
 except NoSuchElementException:
-    print("hittade inte 1")
+    print("hittade inte usa")
 sleep(3)
-# driver.find_element_by_xpath("""//*[@id="sprd-main"]/div[5]/div[2]/div[2]/div[3]/div[1]/img""").click()
-sleep(3)
-try:
-    driver.find_element_by_xpath("""//*[@id="navigation"]/div[3]/div[2]/span""").click()
-except NoSuchElementException:
-    print("hittade inte 2")
+#try:
+    #driver.find_element_by_xpath("""//*[@id="navigation"]/div[3]/div[2]/span""").click()
+#except NoSuchElementException:
+#    print("hittade inte 2")
 #driver.find_element_by_xpath("""//*[@id="navigation"]/div[3]/div[2]/span""").click()
-sleep(3)
+#sleep(3)
 #driver.find_element_by_xpath("""//*[@id="navigation"]/div[3]/div[2]/span""").click()
 # img = driver.find_element_by_xpath("""//*[@id="d-iO"]""")
 #driver.back()
@@ -55,6 +58,7 @@ def navigate(x):
                 """//*[@id="articleTileList"]/div[""" + str(x) + """]/a/div[2]/div[2]""").click()
         except NoSuchElementException:
             print("")
+
         #driver.find_element_by_xpath("""//*[@id="l-i""" + str(x) + """"]""").click()
         #//*[@id="l-i2"]
         #sleep(3)
@@ -74,9 +78,28 @@ def downloadImage(x):
     #driver.refresh()
     #sleep(3)
     driver.execute_script("window.scrollBy(0, 2000);")
+    sleep(4)
+    try:
+        driver.find_element_by_xpath("""//*[@id="navigation"]/div/div[2]/div[2]/div[1]""").click()
+        print("hittade promo")
+    except NoSuchElementException:
+        print("hittade inte promo")
+    except ElementNotInteractableException:
+        print("hittade inte promo")
+    except ElementClickInterceptedException:
+        print("hittade inte promo")
     sleep(2)
-
-    img = driver.find_element_by_xpath("//*[@class='designImage lazyloaded']")
+    try:
+        driver.find_element_by_xpath("""//*[@id="designerSection"]/div[1]/button""").click()
+        print("hittade +")
+    except NoSuchElementException:
+        print("hittade inte +")
+    except ElementNotInteractableException:
+        print("hittade inte +")
+    except ElementClickInterceptedException:
+        print("hittade inte +")
+    sleep(2)
+    img = driver.find_element_by_xpath("""//*[@id="design-info"]/img""")
     #sleep(3)
     src = img.get_attribute('src')
     name = random.randrange(1, 100000)
